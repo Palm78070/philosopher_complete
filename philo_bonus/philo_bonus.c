2e773b05a0c4104c13d0ab8060eb0bb547153104
รัฐ ธรรมธัชอารี <rath@raths-MacBook-Pro.local>
2023-01-14 01:22:10 +0700

    commit3, edit philo.c unused var

--- philo_bonus/philo_bonus.c
+++ philo_bonus/philo_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.c                                            :+:      :+:    :+:   */
+/*   philo_bonus.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rthammat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 18:07:36 by rthammat          #+#    #+#             */
-/*   Updated: 2022/12/24 21:19:54 by rthammat         ###   ########.fr       */
+/*   Updated: 2023/01/14 01:15:10 by rath             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int	main(int argc, char **argv)
 {
 	t_input	*param;
 	t_philo	*ph;
-	int		i;
 
 	if (argc < 5 || argc > 6)
 	{
@@ -27,7 +26,6 @@ int	main(int argc, char **argv)
 	param = (t_input *)malloc(sizeof(t_input));
 	ph = (t_philo *)malloc(sizeof(t_philo));
 	input_init(param, argc - 1, argv);
-	i = -1;
 	struct_init(param, ph);
 	fork_process(ph);
 	return (0);
